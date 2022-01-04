cbb67e2ad0c7f3a53a695834bde5ff8d0fbf03c6
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2022-01-04 16:29:18 +0800

    feat(message): Change axios to async/await

--- src/services/message-service.ts
+++ src/services/message-service.ts
@@ -6,22 +6,21 @@ import { TelegramMessage } from '../types/rest-api'
 /**
  * Greet me
  */
-export function greetMe(message: TelegramMessage): void {
+export async function greetMe(message: TelegramMessage): Promise<void> {
   if (!message || message.text.toLowerCase().indexOf('greetme') < 0) {
     logger.error(`Invalid message: ${JSON.stringify(message)}`)
-    // throw new FailResponse(400, 'Invalid message', message)
   }
 
-  axios
-    .post(`https://api.telegram.org/${envVars.telegramBot.token}/sendMessage`, {
-      chat_id: message.chat.id,
-      text: 'Hello there!!'
-    })
-    .then((res) => {
-      logger.info(`Success sending message: ${JSON.stringify(res)}`)
-    })
-    .catch((err) => {
-      logger.error(`Error while sending message: ${JSON.stringify(err)}`)
-      // throw new ErrorResponse(500, 'Error while sending message', err)
-    })
+  try {
+    const res = await axios.post(
+      `https://api.telegram.org/${envVars.telegramBot.token}/sendMessage`,
+      {
+        chat_id: message.chat.id,
+        text: 'Hello there!!'
+      }
+    )
+    logger.info(`Success sending message: ${JSON.stringify(res)}`)
+  } catch (err) {
+    logger.error(`Error while sending message: ${JSON.stringify(err)}`)
+  }
 }
