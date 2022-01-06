adca884cb44b2073c002dd555e00601126405fd3
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2022-01-06 16:54:22 +0800

    Log all request, not only message

--- src/controllers/webhook-controller/index.ts
+++ src/controllers/webhook-controller/index.ts
@@ -7,7 +7,7 @@ export const frasnymBotWebhook = catchAsync(async function (req, res) {
   // Send message log to FrasNymBotLogger
   axiosService.telegram.sendMessage(
     GROUP_CHAT_ID.FrasNymBotLogger,
-    JSON.stringify(req.body.message)
+    JSON.stringify(req.body)
   )
 
   res.send(new SuccessResponse().serializeResponse())
