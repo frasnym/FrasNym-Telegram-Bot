06bfb25ea62434e6bae1f2a319037da9216c937a
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2022-01-17 14:17:20 +0800

    Refactor: File naming

--- src/controllers/webhook-controller/index.ts
+++ src/controllers/webhook-controller/index.ts
@@ -3,11 +3,11 @@ import { catchAsync } from '../../utils/catch-async'
 import { SuccessResponse } from '../../utils/jsend'
 
 export const sentryBotWebhook = catchAsync(async function (req, res) {
-  await webhookService.handleReceivedMessage(req.body.message)
+  await webhookService.sentryBot.handleReceivedMessage(req.body.message)
   res.send(new SuccessResponse().serializeResponse())
 })
 
 export const zakatSubuhBotWebhook = catchAsync(async function (req, res) {
-  await webhookService.zakatSubuh.handleReceivedMessage(req.body.message)
+  await webhookService.zakatSubuhBot.handleReceivedMessage(req.body.message)
   res.send(new SuccessResponse().serializeResponse())
 })
---
+++ src/services/webhook-service/sentry-bot.ts
@@ -0,0 +1,33 @@
+import envVars from '../../config/envVars'
+import { logger } from '../../config/logger'
+import { TelegramMessage } from '../../types/rest-api'
+import { telegram as axiosTelegramService } from '../axios-service'
+
+/**
+ * Handle received message
+ */
+export async function handleReceivedMessage(
+  message: TelegramMessage
+): Promise<void> {
+  if (!message) {
+    logger.warn(`Invalid message: ${JSON.stringify(message)}`)
+    return
+  }
+
+  try {
+    const loweredCaseMessage = message.text.toLowerCase()
+
+    if (loweredCaseMessage.indexOf('hellobot') >= 0) {
+      const chatResponse = `Hello ${message.from.first_name} 👋\nHow are you today?`
+      await axiosTelegramService.sendMessage(
+        envVars.telegramBot.sentry,
+        message.chat.id,
+        chatResponse
+      )
+    } else {
+      logger.warn(`Unhandled message signal: ${loweredCaseMessage}`)
+    }
+  } catch (err) {
+    logger.error(`Error while sending message: ${JSON.stringify(err)}`)
+  }
+}
